24e79425a9637d595e4a802c5f4030ac991f1517
Harry Parsons <harryjohnparsons@outlook.com>
2025-08-18 22:02:07 +0100

    changes to about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -52,118 +52,111 @@ function About() {
           />
         </div>
         {isLoaded && (
-          <div className="text">
-            <b>
-              <p>Artist Practice:</p>
-            </b>
+          <div className="a-text">
+      
             <br />
             <p>
-              <i>
-                With each artwork, the most important aspect is for my audience
-                to feel a sense of imagination and curiosity.
-                <br />
-                <br />
-                My practice is constantly explorative, each artwork leading to
-                the next, asking new ways of seeing the world each time. I use
-                drawing and sculpture as materials to learn about my
-                surroundings and translate my experiences. The wonder and awe I
-                feel towards nature and human life is what I wish to share with
-                others.
-                <br />
-                <br />
-                Currently, I am most interested in networks and tunnels. I study
-                rabbit warrens, trees, railways, and roads – to name a few. I am
-                fascinated by these passages, and how they energetically connect
-                organisms. Immersive installation is where I can really play and
-                experiment with these themes.
-                <br />
-                <br />
-                The installation ‘The Warren’ is an example of this. The piece
-                comprises of suspended rabbit tunnels, tactile ceramics, and
-                eerie sound recordings. It is a continuous line - looping up and
-                around the building - the gaps filled by our imagination. In
-                this piece I engage with audiences through multiple sensory
-                experiences, evoking nostalgic, childlike creativity.
-                <br />
-                <br />
-                Drawing is foundational to my practice and research process. In
-                the recent collection of drawings, ‘Wiser than us all’, I
-                combine pencil rubbings of old trees with fluid mark making. I
-                work the graphite into the tree’s patterns, carving out the
-                surface of the tree, entwining my story within the tree’s
-                history. The lines and shading constantly change, a never-ending
-                network of shapes that flow from one to another.
-                <br /> <br />
-              </i>
-            </p>
-            <b>
-              <p>Bio:</p>
-            </b>
-            <br />
-            <p>
-              Olivia grew up in rural Devon and moved to London to take up her
-              arts education in 2019. Studying at Camberwell College of Arts
-              UAL, she completed her BA in Fine Art Drawing in 2022. Olivia has
-              shown at Southwark Park Galleries, exhibited a solo collection at
-              Peckham Pelican, and most recently managed a group show of 27
-              emerging artists from her university. She continues her studio
-              practice from her base in Crystal Palace, south-east London.{" "}
-            </p>
-            <br />
-            <b>
-              <p>Exhibitions:</p>
-            </b>
-            <br />
-            <ul>
-              <li>2022 Nunhead Art Exhibition, London. (group)</li>
-              <li>2022 Peckham Pelican, London. (solo)</li>
-              <li>
-                2022 Degree Show, Camberwell College of Arts UAL, London (group)
-              </li>
-              <li>2022 [anti]static, Golden Anchor, London. (group) </li>
-              <li>2022 Out There, Southwark Park Gallery, London. (group)</li>
-              <li>2021 Creative Solutions, Online. (group)</li>
-              <li>2020 Derwent Art Prize 2020, Online. (group)</li>
-              <li>
-                2019 New Camberwell Art, Camberwell College of Arts, London.
-                (group)
-              </li>
-              <li>
-                2019 Exeter Culture Launch, Exeter College of Art, Exeter.
-                (group)
-              </li>
-            </ul>
-            <br />
-            <b>
-              <p>Awards:</p>
-            </b>
-            <br />
-            <ul>
-              <li>2020 Shortlisted, Derwent Art Prize. </li>
-              <li>2019 Special Selection Award, Exeter School of Art</li>
-              <li>
-                2019 Shortlisted, Artist of the Year Award and Grant, Circle
-                Foundation for the Arts.{" "}
-              </li>
-            </ul>
-            <br />
-            <b>
-              <p>Education:</p>
-              <br />
-            </b>
-            <ul>
-              <li>
-                2022 BA Fine Art: Drawing. UAL: Camberwell College of the Arts,
-                London.{" "}
-              </li>
-              <li>
-                2019 Foundation Diploma in Art and Design. Exeter School of Art,
-                Devon.
-              </li>
-            </ul>
+
+              Olivia is a multidisciplinary artist based in West Devon. Through drawing, ceramics, and installation, she explores themes of play, 
+              touch, and the uncanny. In an increasingly digital and technology-driven society, she finds inspiration in her local natural surroundings, 
+              producing tactile works that invite curiosity.
+          </p><br/>
+              <p>
+                Experimentation is central to her practice, both in process and presentation. She is currently focused on creating pieces that encourage 
+                sensory engagement and exhibiting in unconventional spaces.
+            </p><br/>
+              Her recent work explores energetic networks, both natural and human-made. She is particularly interested in the hidden infrastructures such
+               as rabbit warrens, root systems, railways, and footpaths. These passages reveal patterns across species and systems, reflecting our shared 
+               environments and experiences. Her artworks offer a space for reflection and playful curiosity, inviting audiences to slow down and reconnect.
+              <p><br/>
+                Olivia completed a Foundation Diploma in Fine Art at Exeter School of Art in 2018, before relocating to London to undertake a Bachelor 
+                of Arts in Fine Art Drawing at Camberwell College of Arts, University of the Arts London, graduating in 2022. Since then, she has exhibited 
+                in a range of settings, including Southwark Park Gallery in London, Studio KIND in North Devon, and Loughwood Meeting House in East Devon.
+            </p><br/>
+            <hr/>
+            
+           <p><h2><strong>Upcoming Exhibitions</strong></h2></p><br/>
+           <p><strong>Devon Open Studios 2025</strong></p><br/>
+           <p>
+            <strong>Location:</strong> Higher Carley Farm, Lifton, Devon, PL16 0EB
+           </p>
+           <p>
+            <strong>Studio Opening Times:</strong>
+           </p>
+           <br/>
+           
+            <strong>Sat 6 Sept:&nbsp;&nbsp;</strong>10am – 4pm <br/>
+            <strong>Sun 7 Sept:&nbsp;&nbsp;</strong>10am – 4pm <br/>
+            <strong>Fri 12 Sept:&nbsp;&nbsp;</strong>2pm – 6pm <br/>
+            <strong>Sat 13 Sept:&nbsp;&nbsp;</strong>10am – 4pm <br/>
+            <strong>Sun 14 Sept:&nbsp;&nbsp;</strong>10am – 4pm <br/>
+            <strong>Fri 19 Sept:&nbsp;&nbsp;</strong>2pm – 6pm <br/>
+            <strong>Sat 20 Sept:&nbsp;&nbsp;</strong>10am – 4pm <br/>
+            <strong>Sun 21 Sept:&nbsp;&nbsp;</strong>10am – 4pm <br/>
+            <br/>
+            <a href="https://devonartistnetwork.co.uk/studio/olivia-parsons" target="_blank" rel="noopener" style={{textDecoration: 'underline'}}>More details here →</a>
+          <p>
+            <br/>
+                   <hr/>
+                   <br/>
+           <p><strong>Where Are We Now?</strong></p>
+           <br/>
+        <p><strong>Dates:</strong> 6 September - 25 October</p>
+        <p><strong>Location:</strong> Thelma Hulbert Gallery, Honiton, EX14 1LX</p>
+                <p><strong>Opening Times:</strong> Tuesday – Saturday, 10am – 5pm</p>
+        <p >A group exhibition with fellow artists from CAMP (Contemporary Art Membership Platform).</p>
+        <p><a href="https://www.thelmahulbert.com/exhibitions/camp-wherearewenow" target="_blank" rel="noopener" style={{textDecoration: 'underline'}}>More details here →</a></p>
+          </p>
+          <br/>
+        <hr />
+          <br/>
+    <p>
+      <h2>Past Exhibitions</h2>
+      <ul>
+        <li><strong>2025</strong> – <em>Hidden Places</em>, Loughwood Meeting House, Axminster <em>(group)</em></li>
+        <li><strong>2025</strong> – <em>Solo Showcase</em>, Zebediah's Art and Craft Collective, Cornwall <em>(solo)</em></li>
+        <li><strong>2024</strong> – <em>Art in Shop Windows</em>, Launceston Art Weekender, Launceston, Cornwall <em>(group)</em></li>
+        <li><strong>2024</strong> – <em>Summer Open</em>, Studio Kind, Barnstaple <em>(group)</em></li>
+        <li><strong>2024</strong> – Carley Open Studios, Fundraiser for Farms for City Children, Devon <em>(solo)</em></li>
+        <li><strong>2024</strong> – <em>Group Collective</em>, The Paxton Centre, Crystal Palace <em>(group)</em></li>
+        <li><strong>2023</strong> – <em>Solo Showcase</em>, The Ninth Life, London <em>(solo)</em></li>
+        <li><strong>2022</strong> – <em>Nunhead Art Exhibition</em>, Nunhead Community Centre, London <em>(group)</em></li>
+        <li><strong>2022</strong> – <em>Solo Showcase</em>, Peckham Pelican, London <em>(solo)</em></li>
+        <li><strong>2022</strong> – <em>Fine Art Drawing Degree Show</em>, Camberwell College of Arts, University of the Arts London <em>(group)</em></li>
+        <li><strong>2022</strong> – <em>[anti]static</em>, The Golden Anchor, London <em>(group)</em></li>
+        <li><strong>2022</strong> – <em>Out There</em>, Southwark Park Gallery, London <em>(group)</em></li>
+        <li><strong>2021</strong> – <em>Creative Solutions</em>, Online <em>(group)</em></li>
+        <li><strong>2020</strong> – <em>Derwent Art Prize 2020</em>, Online <em>(group)</em></li>
+        <li><strong>2019</strong> – <em>New Camberwell Art</em>, Camberwell College of Arts, University of the Arts London <em>(group)</em></li>
+        <li><strong>2019</strong> – <em>Exeter Culture Launch</em>, Exeter College of Art <em>(group)</em></li>
+      </ul>
+    </p>
+  <br/>
+    <hr />
+    <br/>
+
+    <p>
+      <h2>Awards</h2>
+      <ul class="list">
+        <li><strong>2025</strong> – Devon Open Studios Emerging Artist Bursary</li>
+        <li><strong>2020</strong> – Derwent Art Prize (Shortlisted)</li>
+        <li><strong>2019</strong> – Special Selection Award, Exeter School of Art</li>
+        <li><strong>2019</strong> – Artist of the Year Award and Grant, Circle Foundation for the Arts (Shortlisted)</li>
+      </ul>
+    </p>
+  <br/>
+
+    <hr />
+  <br/>
+
+    <p>
+      <h2>Publication &amp; Press</h2>
+      <p><strong>Interview:</strong> <a href="https://www.youtube.com/watch?v=-FFaLmQQj9s" target="_blank" rel="noopener" style={{textDecoration: 'underline'}}>Art in Me – Watch here</a></p>
+    </p>
           </div>
         )}
       </div>
+
 </div>
     </motion.div>
   );
